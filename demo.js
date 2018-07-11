8660f46137d7217fb20d739b547460d129232632
NDVN_NghiaNT <trongnghiatsq@gmail.com>
2018-07-11 10:32:55 +0700

    trying a.download

--- demo.js
+++ demo.js
@@ -11,12 +11,31 @@
 		}), fileName);
 	};
 
-	document.querySelector('input[type="button"]').addEventListener("touchstart", function(event) {
-		console.log("touchstart");
-		cb(event);
-	});
-	document.querySelector('input[type="button"]').addEventListener("click", function(event) {
+	document.querySelector('#_1').addEventListener("click", function(event) {
 		console.log('click');
 		cb(event);
+	});
+	document.querySelector('#_1').addEventListener("touchstart", function(event) {
+		console.log('touchstart');
+		cb(event);
+	});
+
+	function aDownload(type) {
+		var fileName = 'test.txt';
+		var a = document.createElement("a");
+		document.body.appendChild(a);
+		var file = new Blob([text.value || text.placeholder], { type: type });
+		var fileURL = window.URL.createObjectURL(file);
+		a.href = fileURL;
+		a.download = fileName;
+		a.click();
+	}
+
+	document.querySelector('#_2').addEventListener("touchstart", function(event) {
+		aDownload('text/plain;charset=' + document.characterSet)
+	});
+
+	document.querySelector('#_3').addEventListener("touchstart", function(event) {
+		aDownload('application/octet-stream');
 	});
 }(self));
